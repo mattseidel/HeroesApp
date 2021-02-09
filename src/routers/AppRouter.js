67f615cc9708c0068ee20e91f971085701562c42
MattSeidel <maseidel@misena.edu.co>
2021-02-09 14:47:39 -0500

    routes privates and public added

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,19 +1,27 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {
     BrowserRouter as Router,
     Switch,
     Route,
 } from "react-router-dom";
+import { AuthContext } from '../auth/AuthContext';
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
+import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
+
+    const { user: { logged } } = useContext(AuthContext)
+
     return (
         <Router>
             <div>
                 <Switch>
-                    <Route exact path='/login' component={LoginScreen} />
-                    <Route path='/' component={DashboardRoutes} />
+                    <PublicRoute exact path='/login' component={LoginScreen} isAuthenticated={logged} />
+
+
+                    <PrivateRoute path='/' component={DashboardRoutes} isAuthenticated={logged} />
                 </Switch>
             </div>
         </Router>
